d84957e14dff35bfbd1082b15ba7980a06bef2cf
Leanstix <leanstixx@gmail.com>
2025-01-03 23:42:16 +0100

    fixing production error

--- flow/app/[home]/page.js
+++ flow/app/[home]/page.js
@@ -2,6 +2,14 @@
 import HomePage from "./components/[home]";
 import { useState, useEffect } from "react";
 
+export async function generateStaticParams() {
+  return [
+    { params: { home: 'home1' } },
+    { params: { home: 'home2' } },
+    { params: { home: 'home3' } }
+  ];
+}
+
 export default function UserHome() {
   const [isMounted, setIsMounted] = useState(false);
 
