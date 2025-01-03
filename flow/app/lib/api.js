08faa5062ea95114ca5fef10a46b10fca903c8c4
Leanstix <leanstixx@gmail.com>
2025-01-03 21:30:07 +0100

    fixing production build

--- flow/app/lib/api.js
+++ flow/app/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api'; // Base URL for Django backend
+const API_BASE_URL = '${process.env.NEXT_PUBLIC_API_BASE_URL}/api'; // Base URL for Django backend
 
 const api = axios.create({
   baseURL: API_BASE_URL,
