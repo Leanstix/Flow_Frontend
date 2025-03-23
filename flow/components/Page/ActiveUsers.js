f7dc8809a1ded848a2948ab763216cdddd846acc
Leanstix <leanstixx@gmail.com>
2025-03-23 13:06:49 +0100

    testing the scrollable parts

--- flow/components/Page/ActiveUsers.js
+++ flow/components/Page/ActiveUsers.js
@@ -5,12 +5,14 @@ const ActiveUsers = ({ users }) => {
   return (
     <div className="ml-3">
       <h2 className="font-extrabold text-2xl">Active</h2>
-      {users.map((user, index) => (
-        <div key={index} className="flex mt-2">
-          <Image src={ User } alt={user} width={40} height={40} className="rounded-full"/>
-          <div className="my-3 ml-1">{user}</div>
-        </div>
-      ))}
+      <div className="mt-2">
+        {users.map((user, index) => (
+          <div key={index} className="flex mt-2">
+            <Image src={User} alt={user} width={40} height={40} className="rounded-full"/>
+            <div className="my-3 ml-1">{user}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- flow/components/Page/Announcement.js
+++ flow/components/Page/Announcement.js
@@ -8,7 +8,7 @@ import View from "@/public/Icons/View.svg";
 
 const Announcement = () => {
     return (
-      <div className="border rounded-md bg-[#070007] text-white mt-2 p-2">
+      <div className="border rounded-md bg-[#070007] text-white p-3">
         <h2 className="font-extrabold text-2xl mt-3">Announcement</h2>
         <p className="mt-2">
           Good day dear User, just to notify you that Flow will be unavailable from 12:00 pm to 1:30 pm as we migrate our database to a more secure server. Thank you for your understanding.
