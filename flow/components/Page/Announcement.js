025ca9ab098cf99b95742beb6a08f105c2e94ba8
Leanstix <leanstixx@gmail.com>
2025-03-23 10:47:03 +0100

    fixing Icon

--- flow/components/Page/Announcement.js
+++ flow/components/Page/Announcement.js
@@ -1,3 +1,11 @@
+import Image from "next/image";
+import Like from "@/public/Icons/like_black.svg";
+import Repost from "@/public/Icons/Repost.svg";
+import Comment from "@/public/Icons/Comment.svg";
+import Report from "@/public/Icons/Report.svg";
+import View from "@/public/Icons/View.svg";
+
+
 const Announcement = () => {
     return (
       <div className="border rounded-md bg-[#070007] text-white mt-2 p-2">
@@ -6,6 +14,13 @@ const Announcement = () => {
           Good day dear User, just to notify you that Flow will be unavailable from 12:00 pm to 1:30 pm as we migrate our database to a more secure server. Thank you for your understanding.
         </p>
         <hr className="my-2"/>
+        <div className="grid grid-cols-5 mt-1 mb-5">
+          <Image src={Like} alt="Like" className="col-span-1" width={24} height={24}/>
+          <Image src={Comment} alt="Comment" className="col-span-1" width={24} height={24}/>
+          <Image src={Repost} alt="Repost" className="col-span-1" width={24} height={24}/>
+          <Image src={View} alt="View" className="col-span-1" width={24} height={24}/>
+          <Image src={Report} alt="Report" className="col-span-1" width={24} height={24}/>
+        </div>
       </div>
     );
   };
