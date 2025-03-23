28a7e3f9cc897b3e1da912c6680979876d683475
Leanstix <leanstixx@gmail.com>
2025-03-23 09:07:03 +0100

    fixing posts page

--- flow/components/Page/Post.js
+++ flow/components/Page/Post.js
@@ -1,26 +1,45 @@
 import Image from "next/image";
-import Like from "@/public/Icons/like_black.svg"
-import Repost from "@/public/Icons/Repost.svg"
-import Comment from "@/public/Icons/Comment.svg"
-import Report from "@/public/Icons/Report.svg"
-import View from "@/public/Icons/View.svg"
+import Like from "@/public/Icons/like_black.svg";
+import Repost from "@/public/Icons/Repost.svg";
+import Comment from "@/public/Icons/Comment.svg";
+import Report from "@/public/Icons/Report.svg";
+import View from "@/public/Icons/View.svg";
 
-const Post = ({ name, username, content }) => {
+const Post = ({ name, username, content, likes, reposts, comments, reports, views }) => {
   return (
-    <div className="text-white mt-3">
-      <div className="flex">
+    <div className="text-white mt-3 p-4 bg-[#1F062E] rounded-lg">
+      <div className="flex items-center">
         <Image src="/profile-icon.svg" alt="User" width={50} height={50} className="rounded-full"/>
         <div className="ml-2">
           <div className="font-semibold">{name}</div>
-          <div>@{username}</div>
+          <div className="text-gray-400">@{username}</div>
         </div>
       </div>
+
       <p className="mt-3">{content}</p>
-      <hr className="mt-1"/>
-      <div className="grid grid-cols-5 mt-1 mb-5">
-        {[...Array(5)].map((_, i) => (
-          <Image key={i} src="/icon.svg" alt="Icon" width={40} height={40} className="rounded-full"/>
-        ))}
+      <hr className="mt-2"/>
+
+      <div className="flex justify-between mt-3 text-gray-300">
+        <div className="flex items-center space-x-1">
+          <Image src={Like} alt="Like" width={24} height={24}/>
+          <span>{likes}</span>
+        </div>
+        <div className="flex items-center space-x-1">
+          <Image src={Comment} alt="Comment" width={24} height={24}/>
+          <span>{comments}</span>
+        </div>
+        <div className="flex items-center space-x-1">
+          <Image src={Repost} alt="Repost" width={24} height={24}/>
+          <span>{reposts}</span>
+        </div>
+        <div className="flex items-center space-x-1">
+          <Image src={View} alt="View" width={24} height={24}/>
+          <span>{views}</span>
+        </div>
+        <div className="flex items-center space-x-1">
+          <Image src={Report} alt="Report" width={24} height={24}/>
+          <span>{reports}</span>
+        </div>
       </div>
     </div>
   );
