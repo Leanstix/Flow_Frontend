c1d032ab97279646680f5ba3cf849ef9d7e12acd
Leanstix <leanstixx@gmail.com>
2025-03-23 12:38:43 +0100

    testing the scrollable parts

--- flow/components/Page/ActiveUsers.js
+++ flow/components/Page/ActiveUsers.js
@@ -7,11 +7,8 @@ const ActiveUsers = ({ users }) => {
       <h2 className="font-extrabold text-2xl">Active</h2>
       {users.map((user, index) => (
         <div key={index} className="flex mt-2">
-          <div className="overflow-y-scroll h-1/2">
-            <Image src={ User } alt={user} width={40} height={40} className="rounded-full"/>
-            <div className="my-3 ml-1">{user}</div>
-          </div>
-          
+          <Image src={ User } alt={user} width={40} height={40} className="rounded-full"/>
+          <div className="my-3 ml-1">{user}</div>
         </div>
       ))}
     </div>
