73505bfa35e2b53a402d9f310cdca86e6934ec4b
Leanstix <leanstixx@gmail.com>
2025-03-23 10:38:38 +0100

    fixing Icon

--- flow/components/Page/FriendRequest.js
+++ flow/components/Page/FriendRequest.js
@@ -4,7 +4,7 @@ import User from "@/public/User.svg";
 const FriendRequest = ({ username }) => {
   return (
     <div className="bg-[#1F062E] p-4 rounded-lg text-center">
-      <Image src={ User } alt="User" width={60} height={60} className="rounded-full"/>
+      <Image src={ User } alt="User" width={60} height={60} className="rounded-full justify-self-center"/>
       <p className="text-white font-extrabold">{username}</p>
       <button className="bg-[#1F062E] hover:border p-1 mt-2 w-full rounded-md text-white font-bold">Accept</button>
       <button className="bg-[#1F062E] hover:border p-1 mt-1 w-full rounded-md text-white font-bold">Delete</button>
