d2c13abd00da9cb32ca535026a5026e5c79ad5f1
Leanstix <leanstixx@gmail.com>
2025-03-24 16:54:09 +0100

    making height adjustments

--- flow/components/Page/Layout.js
+++ flow/components/Page/Layout.js
@@ -32,6 +32,9 @@ const Layout = () => {
               </div>
               <Post name="John Doe" username="johndoe" content="Great developers don’t just write code—they architect solutions." />
               <Post name="Jane Doe" username="janedoe" content="Build with purpose and make an impact." />
+              <Post name="John Doe" username="johndoe" content="Great developers don’t just write code—they architect solutions." />
+              <Post name="Jane Doe" username="janedoe" content="Build with purpose and make an impact." />
+              
             </>
           )}
           
